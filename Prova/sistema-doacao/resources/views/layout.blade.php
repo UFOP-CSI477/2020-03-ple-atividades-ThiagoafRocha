57f08bf5b02fa0dc3a70562cfed0c82449604e33
Thiago Rocha <thiago_afr@hotmail.com>
2020-10-03 12:49:06 -0300

    1.2 Página inicial

---
+++ Prova/sistema-doacao/resources/views/layout.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('pagina-nome')</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+    integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a href="/" class="navbar-brand">LOGO</a>
+
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-tem active">
+                        <a href="#" class="nav-link">Área Geral</a>
+                    </li>
+                    <li class="nav-tem active">
+                        <a href="#" class="nav-link">Área Administrativa</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+
+    <div class="container">
+        @yield('conteudo')
+    </div>
+</body>
+</html>
---
+++ Prova/sistema-doacao/resources/views/pages/principal.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('pagina-nome')
+    Pagina Principal - Doe Sangue!
+@endsection
+
+@section('conteudo')
+    <h1>Página Principal</h1>
+@endsection
